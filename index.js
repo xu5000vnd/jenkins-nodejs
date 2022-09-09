80cf6788d19145b97e591656edfe388ed9103d75
horison <horison@oryton.com>
2022-09-09 18:08:14 +0700

    update

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-require('./src/server');
+const express = require('express');
+const cors = require('cors');
+
+const app = express();
+app.use(cors());
+
+app.get('/', (req, res) => {
+  res.status(200).json({ message: "Hello there!!!!" });
+})
+
+const port = process.env.PORT || 3000;
+app.listen(port, function () {
+  console.log('listening on port ' + port);
+});
